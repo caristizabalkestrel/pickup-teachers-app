ddcb0201427b8399088cd54d63e59174abd06499
Camilo Aristizabal Bedoya <camilo_aristizabal@outlook.com>
2025-05-21 22:28:13 -0500

    configuracion de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDgdh7yfTceN9k2z1waB-4do13WL91iGI8",
+    authDomain: "moviles2-95f4d.firebaseapp.com",
+    projectId: "moviles2-95f4d",
+    storageBucket: "moviles2-95f4d.firebasestorage.app",
+    messagingSenderId: "635509387734",
+    appId: "1:635509387734:web:0716be32fa76166ff5b477"
+  },
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
